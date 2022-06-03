02c5d7ca79b3409ce2f16a11dc591de6fc6315dd
Ose-Augustine <osebeckley@gmail.com>
2022-06-03 19:06:43 +0100

    Changes
    Changed the model name from form to person just to avoid clashing with FlaskForm.
    Filtered out the routes and handlers.
    Rest is debugging purposes.

--- app.py
+++ app.py
@@ -1,32 +1,36 @@
 from setuptools import setup
-from models import setup_db, Form 
+from models import setup_db, Person 
 from forms import BasicForm
 from flask import Flask, render_template, request, redirect, url_for,flash
 
-app = Flask(__name__) 
+app = Flask(__name__)
+app.config['SECRET_KEY']='bahd'
 setup_db(app)
 
-@app.route('/')
-def index():
-    form = BasicForm()
-    return render_template('index.html',form=form)
+@app.route('/forms')
+def render_forms():
+    form = BasicForm() 
+    return render_template('forms.html',form=form)
 
 @app.route('/submissions',methods=['POST'])
-def form_submission():
+def create_form_submission():
     form = BasicForm(request.form)
-    if form.validate_on_submit():
-        person = Form(
-            first_name=form.first_name.data,
-            last_name = form.last_name.data,
-            password  = form.password.data, 
-            email = form.email.data 
-        )
-        person.insert()
-        flash("Submission was a success")
+    person = Person(
+        first_name=form.first_name.data,
+        last_name = form.last_name.data,
+        password  = form.password.data ,
+        email = '',
+    )
+    if form.validate_on_submit:
+        try:
+            person.insert()
+        except:
+            person.reverse()
+            return 'problemS'
     else:
-        return "<h1>An error occured<h1> "
-    return redirect(url_for('index'))
-
+        for error in form.errors:
+            return error
+    return redirect(url_for('render_forms'))
 
 if __name__=='__main__':
     app.run(debug=True)
