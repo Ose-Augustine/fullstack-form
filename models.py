542b07a6cceaa38345911dbb25f34c01faa3b1ab
Ose-Augustine <osebeckley@gmail.com>
2022-06-03 09:47:54 +0100

    Setting up the models.py for database config

---
+++ models.py
@@ -0,0 +1,34 @@
+from flask_sqlalchemy import SQLAlchemy 
+from sqlalchemy import Column, Integer, String 
+
+database_name = "fullstacks"
+database_path = "postgresql://{}:{}@{}/{}".format(
+    "augustine","bahdman","localhost:5432",database_name
+)
+
+db = SQLAlchemy()
+
+def setup_db(app,database_path=database_path):
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+    app.config['SQLALCHEMY_DATABASE_URI']=database_path
+    db.init_app(app)
+    db.create_all()
+
+class Form(db.model):
+    id = Column(Integer,primary_key=True)
+    first_name = Column(String(),nullable=False)
+    last_name =Column(String(),nullable=False)
+    password = Column(Integer(),nullable=False)
+    email  =Column(String(),nullable=False)
+
+    def __init__(self,id,first_name,last_name,password,email):
+        id         = self.id 
+        first_name = self.first_name
+        last_name  = self.last_name
+        password   = self.password
+        email      = self.email 
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+        db.session.close()
