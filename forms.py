9f43118e2aa5fbc5fa9f6bfa6e4901a90d4a8e4e
Ose-Augustine <osebeckley@gmail.com>
2022-06-13 14:49:40 +0100

    Added validation to the phonenumber field.

--- forms.py
+++ forms.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm as Form
+import re 
 from traitlets import validate 
 from wtforms import StringField,PasswordField,EmailField,TelField
-from wtforms.validators import DataRequired,InputRequired, Email , EqualTo
+from wtforms.validators import DataRequired,InputRequired, Email , EqualTo, ValidationError
 
 class BasicForm(Form):
     phone_number = TelField('phone_number',validators=[DataRequired()])
@@ -10,3 +11,11 @@ class BasicForm(Form):
     password = PasswordField('password',validators=[DataRequired()])
     confirm = PasswordField('confirm_password',validators=[EqualTo("password",message="passwords must be the same")])
     email = EmailField('email',validators=[Email(message="This is email is not of the right format ")])
+
+    def validate_phone_number(self,phone_number):
+        number= phone_number.data
+        pattern = re.compile(r'\d{3}?-\d{3}-\d{3}')
+        matches = pattern.finditer(number)
+        if number != pattern:
+            self.phone_number.errors += (ValidationError("Wrong Phone number format"),)
+        
